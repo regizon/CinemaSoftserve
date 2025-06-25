ac981203611f0a8b29a374a9a1d03d4764fea4b1
Bassdik <danilhlapov@gmail.com>
2025-06-25 13:35:35 +0300

    Basic structure
    /index/ - main page
    
    static - folder for images, css etc.
    static`s structure:
    static/css
    static/img/movies
    static/img/icons
    
    class IndexView - simple view class

--- backend/cinema/public_api/views.py
+++ backend/cinema/public_api/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from cinema.models import Movie, Booking, Genre, User
 from cinema.public_api.serializers import MovieSerializer, BookingSerializer, GenreSerializer, RegisterSerializer
 
+from django.views.generic import TemplateView
+
 class PublicMovieViewset(viewsets.ReadOnlyModelViewSet):
     queryset = Movie.objects.filter(is_active=True)
     serializer_class = MovieSerializer
@@ -22,3 +24,6 @@ class PublicUserBooking(generics.CreateAPIView):
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
--- backend/cinema/urls.py
+++ backend/cinema/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 from cinema.admin_api.views import AdminMovieViewset
-from cinema.public_api.views import PublicMovieViewset, PublicUserBooking, RegisterView
+from cinema.public_api.views import PublicMovieViewset, PublicUserBooking, RegisterView, IndexView
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -13,6 +13,7 @@ router = routers.SimpleRouter()
 router.register(r'public/movies', PublicMovieViewset,basename='public')
 router.register(r'admin/movies', AdminMovieViewset, basename='admin')
 urlpatterns = [
+    path('index/', IndexView.as_view()),
     path('api/v1/', include(router.urls)),
     path('api/v1/bookings', PublicUserBooking.as_view()),
     path('register/', RegisterView.as_view(), name='register'),
