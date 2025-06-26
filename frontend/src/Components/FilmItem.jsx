ddbdf0d05c17d7852091def504b4c41f24788956
Bassdik <danilhlapov@gmail.com>
2025-06-26 21:59:22 +0300

    FilmGrid fix
    main page film grid requires:
    img_url,
    title,
    description (change to year, country and genres)

--- frontend/src/Components/FilmItem.jsx
+++ frontend/src/Components/FilmItem.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function FilmItem({ img, title, info }) {
+export default function FilmItem({ img_url, title, description }) {
   return (
     <div className="poster">
       <div className="film-item">
-        <img src={img} alt={title} />
+        <img src={img_url} alt={title} />
         <div className="film-caption">{title}</div>
-        <div className="film-caption2">{info}</div>
+        <div className="film-caption2">{description}</div>
       </div>
     </div>
   );
